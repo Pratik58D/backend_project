89264415b0ed061ad643e57c3d7c8babd89afe0e
Pratik58D <pratikdevkota58@gmail.com>
2025-08-24 19:01:41 +0545

    google location tracker using leaflet and socketio

---
+++ real time tracker/public/js/script.js
@@ -0,0 +1,55 @@
+
+const socket = io();
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude,longitude}=position.coords;
+        console.log(latitude,longitude)
+        socket.emit("send-location",{latitude,longitude})
+    },(error)=>{
+        console.error(error);
+        
+    },{
+        enableHighAccuracy : true,
+        timeout : 5000,
+        maximumAge : 0
+    }
+
+
+);
+}
+
+const map = L.map("map").setView([0,0],15);
+
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png",{
+    attribution :"real tracking Pratik "
+
+}).addTo(map);
+
+const markers ={};
+
+
+socket.on("recieve-location",(data)=>{
+
+    const {id,latitude,longitude} = data;
+
+    map.setView([latitude,longitude])
+
+    if(markers[id]){
+        markers[id].setLatLng([latitude,longitude]);
+
+    }else{
+        markers[id] = L.marker([latitude,longitude]).addTo(map);
+    }
+
+})
+
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+
+    }
+
+})
---
+++ real time tracker/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import http from "http";
+import {Server} from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine" , "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",(socket)=>{
+    console.log("connected")
+
+
+    socket.on("send-location" , (data)=>{
+        io.emit("recieve-location",{id:socket.id , ...data})
+    });
+
+    socket.on("disconnect",function(){
+        io.emit("user-disconnected", socket.id)
+
+    })
+
+
+
+})
+
+
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+
+
+server.listen(5000 , "0.0.0.0", ()=>{
+    console.log("server is running...")
+})
