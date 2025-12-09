3461b404db7ee77c7944482bb7f38ac2c91a6812
Pratik58D <pratikdevkota58@gmail.com>
2025-12-09 22:57:46 +0545

    data struncture List

--- node js ALL/Redis/dataStructure.js
+++ node js ALL/Redis/dataStructure.js
@@ -13,24 +13,46 @@ client.on("error",(error)=> console.log("redis client error",error));
 async function redisDataStruncture(){
     try {
 
-         await client.connect();
+        await client.connect();
         console.log("conected to redis");
         
 
 
-        // string -> SET , GET , MSET , MGET
+        //1.......... string -> SET , GET , MSET , MGET..............
 
-        await client.set("user:name" , "virat kohli")
-        const name = await client.get("user:name")
-        console.log({name})
+        // await client.set("user:name" , "virat kohli")
+        // const name = await client.get("user:name")
+        // console.log({name})
 
         // multiple value set
 
-        await client.MSET(["user:email" , "virat@gmail.com" , "user:age" , "70" , "user:country", "Nepal"])
+        // await client.MSET(["user:email" , "virat@gmail.com" , "user:age" , "70" , "user:country", "Nepal"])
 
-        const [email , age , country] = await client.MGET(["user:email" ,"user:age","user:country"])
-        console.log({email}, {age} , {country})
+        // const [email , age , country] = await client.MGET(["user:email" ,"user:age","user:country"])
+        // console.log({email}, {age} , {country})
 
+
+        // 2...........lists -> Lpush ,Rpush , Lrange , lpop , rpop...................
+
+        // client.lPush: This command is used to insert one or more values at the head (left side) of the List stored at the key "notes"
+        // jati choti code chalxa push element memory ma store hudai janxa
+
+        // await client.lPush("notes",["ram","sham","hari"]);
+        // const extractNotes = await client.lRange("notes",0 ,1)
+        // const extractNotes = await client.lRange("notes",0 ,-1)
+        // console.log(extractNotes)
+
+     //Removes and returns the element at the Head (Left) of the list.
+      //List: [A, B, C] Returns 'A', List becomes [B, C]
+       
+    //    const firstNote = await client.lPop("notes");
+    //     console.log({firstNote})             //hari
+
+    //     const remainNotes = await client.lRange("notes",0,-1);
+    //     console.log({remainNotes})
+
+       
+       //3.........sets => SADD, SMEMBERS,SISMEMBER , SREM.......
 
         
     } catch (error) {
